4cf0991dd92f9629ff03f1d0762b2b392466b7f4
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-04-07 00:49:00 +0530

    Fix env

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -2,13 +2,15 @@ import { createAuthClient } from "better-auth/react"
 import { oneTapClient } from "better-auth/client/plugins";
 
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID as string
+
 export const authClient = createAuthClient({
   baseURL: typeof window !== 'undefined'
     ? window.location.origin
     : process.env.BETTER_AUTH_URL || 'http://localhost:3000',
   plugins: [
     oneTapClient({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientId: googleClientId,
       autoSelect: false,
       cancelOnTapOutside: true,
       context: "signin",
